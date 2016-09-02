9fdc1df0cf4b3b1ad12818a5cd6cf9ad33c07c66
loggerhead <lloggerhead@gmail.com>
2016-09-02 14:00:13 +0800

    finished proxy tests

--- tests/common_proxy.rs
+++ tests/common_proxy.rs
@@ -6,23 +6,46 @@ use std::process::{Command, Output};
 use shadowsocks::config;
 use shadowsocks::relay::Relay;
 
+
+const URLS: &'static [&'static str] = &[
+    "https://www.baidu.com/",
+    "https://news.ycombinator.com/news/",
+    "http://test-ipv6.com/",
+];
+
+
 #[test]
 fn main() {
-    let tests = vec![
-        "www.baidu.com",
-        // "www.qq.com"
-    ];
-
     // TODO: change to get args from command line
-    let _threads = vec![
-        start_client("tests/config/local_conf.toml"),
-        start_server("tests/config/server_conf.toml"),
-    ];
+    start_client("tests/config/local_conf.toml");
+    start_server("tests/config/server_conf.toml");
 
-    for test in tests {
-        let res1 = run_curl(test, None);
-        let res2 = run_curl(test, Some("127.0.0.1:8488"));
-        assert_eq!(res1, res2);
+    let threads = URLS.iter().map(|url| {
+        thread::spawn(move || {
+            let res1 = run_curl(url, None);
+            let res2 = run_curl(url, Some("127.0.0.1:8488"));
+            assert_eq!(res1.status, res2.status, "{}", url);
+            assert!(res1.stdout == res2.stdout, "output of curl not equal: {}", url);
+
+            // if res1.stdout != res2.stdout {
+            //     use std::str;
+            //     use std::fs::File;
+            //     use std::io::prelude::*;
+            //
+            //     let res1 = unsafe {str::from_utf8_unchecked(&res1.stdout)};
+            //     let res2 = unsafe {str::from_utf8_unchecked(&res2.stdout)};
+            //
+            //     let mut f1 = File::create("res1.txt").unwrap();
+            //     let mut f2 = File::create("res2.txt").unwrap();
+            //     f1.write_all(res1.as_bytes()).unwrap();
+            //     f2.write_all(res2.as_bytes()).unwrap();
+            //     assert!(false, "{}", url);
+            // }
+        })
+    });
+
+    for t in threads {
+        t.join().unwrap();
     }
 }
 
@@ -46,13 +69,12 @@ fn start_server(config_path: &str) -> thread::JoinHandle<()> {
 fn run_curl(url: &str, proxy: Option<&str>) -> Output {
     let mut cmd = Command::new("curl");
     cmd.arg(url).arg("-v").arg("-L")
-       .arg("-m").arg("15")
-       .arg("--connect-timeout").arg("10");
+       .arg("-m").arg("30")
+       .arg("--connect-timeout").arg("20");
 
     if proxy.is_some() {
         cmd.arg("--socks5-hostname").arg(proxy.unwrap());
     }
 
-    cmd.spawn().expect("failed to execute cmd")
-       .wait_with_output().expect("failed to wait on child")
+    cmd.output().expect("failed to execute cmd")
 }
