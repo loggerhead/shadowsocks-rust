3b03f6a39fb362ddc1330d0f88f4fea015f2600a
loggerhead <lloggerhead@gmail.com>
2016-11-27 10:02:25 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 curl https://sh.rustup.rs -sSf | sh
 . $HOME/.cargo/env
 git clone https://github.com/loggerhead/shadowsocks-rust.git && cd shadowsocks-rust/
-cargo build --release --features "$SS_FEATURES" && mv target/release/ssserver ssserver
-cargo build --release --features "sslocal $SS_FEATURES" && mv target/release/ssserver sslocal
+cargo build --release --features "$SS_FEATURES" && mv target/release/ssserver ../ssserver
+cargo build --release --features "sslocal $SS_FEATURES" && mv target/release/ssserver ../sslocal
+cd ..
 ./ssserver --version
 ./sslocal --version
